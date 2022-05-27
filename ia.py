7bc93c4e6adda8ba42250c88aee0bc0a913618a9
Bubulle <bubulle86@yahoo.fr>
2022-05-27 11:40:46 +0200

    made code cleaner

--- ia.py
+++ ia.py
@@ -5,6 +5,7 @@ from range_xd import RangeXD
 import math
 import treePlot
 
+# All possible win line (start x, start y, move x, move y)
 WIN_LINES = [
     # Horizontal line
     (0, 0, 1, 0),
@@ -18,14 +19,27 @@ WIN_LINES = [
     (0, 0, 1, 1),
     (0, 2, 1, -1),
 ]
+
+# stock all the already calculated win score of board
 board_value_cache = {}
 
 
 def get_move_win_rate(board, depth=0, tree: treePlot.Tree = None, previous_play=None):
+    """
+    Give the win rate of all possible move
+    :param board:
+    :param depth:
+    :param tree:
+    :param previous_play:
+    :return dictionary of the move a the win value:
+    """
+
     empty_cell, player_turn = get_next_move(board)
+
     if previous_play is None:
         previous_play = ()
-    if len(empty_cell) == 0:
+
+    if len(empty_cell) == 0:    # Check is the board in full or not
         return {}
 
     move_score = {}
@@ -33,23 +47,29 @@ def get_move_win_rate(board, depth=0, tree: treePlot.Tree = None, previous_play=
         tree.add_node(previous_play, (board, 0))
 
     for play in empty_cell:
+        # Duplicate the board and apply the current play
         new_board = board.copy()
         new_board[play] = player_turn
+
+        # Get the board hash and check if the board has already been calculated
         board_hash = get_board_hash(new_board)
         if board_hash in board_value_cache.keys():
             move_score[play] = board_value_cache[board_hash]
             continue
 
-        new_board_score = get_board_score(new_board)
+        new_board_score = get_board_score(new_board)  # Check if the board is a winning board
         if new_board_score == 0:
-            next_move_scores = get_move_win_rate(new_board, depth=depth + 1, tree=tree, previous_play=previous_play + (play,))
-            if len(next_move_scores) != 0:
-
+            # Get the score of the board by resistivity
+            next_move_scores = get_move_win_rate(new_board, depth=depth + 1, tree=tree,
+                                                 previous_play=previous_play + (play,))
+            if len(next_move_scores) != 0:  # Check if is the last play
+                # Get the best enemy play
                 new_board_score = min([score * player_turn for score in next_move_scores.values()]) \
-                                  * (1 if depth % 2 == 0 else 1) * player_turn
+                                  * player_turn
             else:
                 new_board_score = 0
-        board_value_cache[get_board_hash(new_board)] = new_board_score
+
+        board_value_cache[get_board_hash(new_board)] = new_board_score  # Put the board score in the cache
         move_score[play] = new_board_score
         if tree is not None:
             tree.add_node(previous_play + (play,), (new_board_score, new_board))
@@ -57,10 +77,21 @@ def get_move_win_rate(board, depth=0, tree: treePlot.Tree = None, previous_play=
 
 
 def get_board_hash(board):
+    """
+    Returns a unique number of the board
+    :param board:
+    :return a unique number:
+    """
     board_string = "".join([str(value + 1) for value in board.values()])
     return int(board_string, 3)
 
+
 def get_next_move(board):
+    """
+    Returns all the possible move and who is the players at play
+    :param board:
+    :return all empty cell and the player turn:
+    """
     player_turn = 1
     empty_cell = []
     for pos in RangeXD(3, 3):
@@ -73,16 +104,26 @@ def get_next_move(board):
 
 
 def get_board_score(board):
+    """
+    Returns whether the board is a winner or not
+    :param board:
+    :return 1, -1 or 0:
+    """
     scores = []
-    for start_x, start_y, vel_x, vel_y in WIN_LINES:
+    for start_x, start_y, vel_x, vel_y in WIN_LINES:    # Check each line
         line_content = []
-        for i in range(3):
+        for i in range(3):  # Put the content of each line in a list
             line_content.append(board[start_x + vel_x * i, start_y + vel_y * i])
-        scores.append(get_line_score(line_content))
-    return min(max(sum(scores), -1), 1)
+        scores.append(get_line_score(line_content))  # Get the win value of that line
+    return min(max(sum(scores), -1), 1)  # Calculate if the board is win or not
 
 
 def get_line_score(line):
+    """
+    Returns the score of a line
+    :param line:
+    :return 1, -1 or 0:
+    """
     assert len(line) == 3
 
     if line in [[-1, -1, -1], [1, 1, 1]]:
@@ -155,7 +196,7 @@ def play_game():
     board = {pos: 0 for pos in RangeXD(3, 3)}
     i = 0
     turn = 1
-    ia_turn = -1
+    ia_turn = 1
     while get_board_score(board) == 0 and i != 9:
         if turn == ia_turn:
             ia_plays = get_move_win_rate(board)
